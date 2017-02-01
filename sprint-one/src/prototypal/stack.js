321fdbef57df86597cf19bbc3d85945f520f16c7
Hack Reactor Students <accounts@hackreactor.com>
2017-02-01 15:45:07 -0600

    Implements prototypal class for stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
+  var stack = Object.create(stackMethods);
+  stack.length = 0;
+  stack.storage = {};
+
+
+  return stack;
+
+
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function () {
+    return this.length;
+  },
+  push: function (val) {
+    this.storage[this.length] = val;
+    this.length++;
+  },
+  pop: function () {
+    var popped = this.storage[this.length - 1];
+    if (this.length > 0) {
+      this.length--;
+    }
+    return popped;
+  }
+};
 
 
