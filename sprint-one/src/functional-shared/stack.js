78ce2709350668cacba5d8efaf7858744bfb5bd2
Hack Reactor Students <accounts@hackreactor.com>
2017-02-01 12:30:12 -0600

    Implements the functional-shared pattern for stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
+  var newStack = {length: 0, storage: {} };
+  _.extend(newStack, stackMethods);
+
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function () {
+    return this.length;
+  },
+  push: function (value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  pop: function () {
+    var popped = this.storage[this.length - 1];
+    if (this.length > 0) {
+      this.length --;
+    }
+    return popped;
+  }
+
+};
 
 
