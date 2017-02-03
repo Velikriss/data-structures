fc32c85625fbfcb611ba7b912d02ea6b4cd691a5
Hack Reactor Students <accounts@hackreactor.com>
2017-02-02 20:11:15 -0600

    Initiates hash table insert function to near complete state

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,54 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var pair = {k: v, next: null};
+  if (!this._storage.get(index)) {
+    this._storage.set(index, pair);
+  } else {
+    var head = this._storage.get(index);
+    var traversal = head;
+    while (traversal.next !== null) {
+      traversal = traversal.next;
+    }
+    traversal.next = pair;
+    this._storage.set(index, head);
+    this._storage.set(index, traversal);
+    
+    if (v === 'val2') {
+      debugger;
+    }
+    console.log(this._storage.get(index));
+  }
+
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+ //  var index = getIndexBelowMaxForKey(k, this._limit);
+ //  var retrieved = this._storage.get(index);
+ //  if (retrieved === undefined) {
+ //    return undefined;
+ //  }
+ //  var key = retrieved.slice(0, retrieved.indexOf('^@'));
+ // // console.log(key);
+ //  if (key === k) {
+ //    var value = retrieved.slice(retrieved.indexOf('^@') + 2, retrieved.length);
+ //    return value;
+ //  } else {
+ //    return 0;
+ //  }
+ //return 0;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
+};
+
+HashTable.prototype.checkKeyValue = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  
+
 };
 
 
